21e691a8aeefdc7cd8e88db77b21a74440f22f71
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2020-05-02 22:36:44 -0300

    Background

--- src/components/layout/styles.js
+++ src/components/layout/styles.js
@@ -1,12 +1,14 @@
+import bg from '../../images/bg.gif'
 import styled from 'styled-components';
 
 export const LayoutContainer = styled.div`
   min-height: 100vh;
+  background-image: url(${bg});
+  background-size: cover;
 `;
 
 export const LayoutContent = styled.main`
   display: flex;
   flex-direction: column;
   min-height: 100vh;
-  background-color: teal;
 `;
